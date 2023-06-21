336ad5809cda8fa7e78db8196dd367805c0318de
CaioQuinteiro <caioquinteiro7@gmail.com>
2023-06-21 18:07:42 -0300

    Update pedidos.sql

--- pedidos.sql
+++ pedidos.sql
@@ -1,29 +1,171 @@
-CREATE TABLE CLIENTE (
-    id INT PRIMARY KEY,
-    nome VARCHAR(50),
-    endereco VARCHAR(100),
-    telefone VARCHAR(15)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 21/06/2023 às 23:06
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.0.28
 
-CREATE TABLE PRODUTO (
-    id INT PRIMARY KEY,
-    nome VARCHAR(50),
-    preco DECIMAL(10, 2),
-    qtde INT
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE PEDIDO (
-    id INT PRIMARY KEY,
-    id_cliente INT,
-    data_pedido DATE,
-    FOREIGN KEY (id_cliente) REFERENCES CLIENTE(id)
-);
 
-CREATE TABLE ITEMPEDIDO (
-    id INT PRIMARY KEY,
-    id_pedido INT,
-    id_produto INT,
-    qtde INT,
-    FOREIGN KEY (pedido_id) REFERENCES PEDIDO(id),
-    FOREIGN KEY (produto_id) REFERENCES PRODUTO(id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `pedidos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `ID` int(11) NOT NULL,
+  `NOME` varchar(50) NOT NULL,
+  `ENDERECO` varchar(100) NOT NULL,
+  `TELEFONE` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+--
+-- Despejando dados para a tabela `cliente`
+--
+
+INSERT INTO `cliente` (`ID`, `NOME`, `ENDERECO`, `TELEFONE`) VALUES
+(1, 'Caio Quinteiro', 'Rua Aparecido De Almeida, 377', '(18) 99655-6066'),
+(2, 'Nilton Duarte', 'Rua Joao Ferreira, 254', '(18) 99633-9332');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `itens_pedido`
+--
+
+CREATE TABLE `itens_pedido` (
+  `ID` int(11) NOT NULL,
+  `ID_PEDIDO` int(11) NOT NULL,
+  `ID_PRODUTO` int(11) NOT NULL,
+  `QUANTIDADE` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `ID` int(11) NOT NULL,
+  `ID_CLIENTE` int(11) NOT NULL,
+  `DATA_PEDIDO` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `ID` int(11) NOT NULL,
+  `NOME` varchar(50) NOT NULL,
+  `PRECO` float NOT NULL,
+  `QTDE` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+--
+-- Despejando dados para a tabela `produto`
+--
+
+INSERT INTO `produto` (`ID`, `NOME`, `PRECO`, `QTDE`) VALUES
+(1, 'Mouse RedDragon', 159, 14),
+(2, 'Teclado RedDragon', 249, 15),
+(3, 'Monitor AOC', 800, 5);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Índices de tabela `itens_pedido`
+--
+ALTER TABLE `itens_pedido`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `ID_PEDIDO` (`ID_PEDIDO`),
+  ADD KEY `ID_PRODUTO` (`ID_PRODUTO`);
+
+--
+-- Índices de tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `ID_CLIENTE` (`ID_CLIENTE`);
+
+--
+-- Índices de tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `itens_pedido`
+--
+ALTER TABLE `itens_pedido`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `itens_pedido`
+--
+ALTER TABLE `itens_pedido`
+  ADD CONSTRAINT `ID_PEDIDO` FOREIGN KEY (`ID_PEDIDO`) REFERENCES `pedidos` (`ID`),
+  ADD CONSTRAINT `ID_PRODUTO` FOREIGN KEY (`ID_PRODUTO`) REFERENCES `produto` (`ID`);
+
+--
+-- Restrições para tabelas `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `ID_CLIENTE` FOREIGN KEY (`ID_CLIENTE`) REFERENCES `cliente` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
