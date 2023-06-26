1bdb9e9c3e489dc3acc7865a7072f0da8b652867
CaioQuinteiro <caioquinteiro7@gmail.com>
2023-06-26 19:06:55 -0300

    Update recinsCliente.php

--- VIEW/cliente/recinsCliente.php
+++ VIEW/cliente/recinsCliente.php
@@ -6,7 +6,7 @@
     $cliente = new \MODEL\Cliente();
 
     $cliente->setNome($_POST['nome']);
-    $cliente->setEndereco($_POST['endereo']);
+    $cliente->setEndereco($_POST['endereco']);
     $cliente->setTelefone($_POST['telefone']);
 
     $bll = new \BLL\bllCliente();
